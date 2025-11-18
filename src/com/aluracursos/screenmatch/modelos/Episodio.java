864adc3b4df1a9eb1616c6d0b38c4a22c113b025
Rortega <rojosormi@gmail.com>
2025-11-18 13:25:53 -0500

    Nuevos archivos par el proyecto en Java

---
+++ src/com/aluracursos/screenmatch/modelos/Episodio.java
@@ -0,0 +1,31 @@
+package com.aluracursos.screenmatch.modelos;
+
+public class Episodio {
+    private int numero;
+    private String nombre;
+    private String serie;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSerie() {
+        return serie;
+    }
+
+    public void setSerie(String serie) {
+        this.serie = serie;
+    }
+}
